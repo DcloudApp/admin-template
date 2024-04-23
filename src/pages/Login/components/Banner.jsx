e8ef16986989d3d782f192fa76e87078c0e4c738
420045389 <420045389@qq.com>
2024-04-23 17:14:08 +0800

    ✨ feat:调整登录页布局

--- src/pages/Login/components/Banner.jsx
+++ src/pages/Login/components/Banner.jsx
@@ -1,10 +1,15 @@
+import { required } from '@/utils/index'
+
 export default defineComponent({
   props: {},
   emits: [],
   setup() {
     return () => (
       <>
-        <div className="flex flex-col items-center"></div>
+        <div className="h-full w-full flex flex-col items-center justify-center">
+          <img className="w-300px flex dark:hidden" src={required('@/assets/img/auth-v2-forgot-password-illustration-light.png')} />
+          <img className="hidden w-300px dark:flex" src={required('@/assets/img/auth-v2-forgot-password-illustration-dark.png')} />
+        </div>
       </>
     )
   },
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -9,10 +9,10 @@ export default defineComponent({
       <>
         <div className="h-100vh w-full flex flex-col bg-[var(--color-bg-1)]">
           <div className="flex flex-1">
-            <div className="hidden w-550px md:flex">
+            <div className="hidden flex-1 md:flex">
               <Banner />
             </div>
-            <div className="flex flex-1 flex-col items-center justify-center">
+            <div className="w-full flex flex-col items-center justify-center md:w-500px md:items-start">
               <div class="content-inner">
                 <LoginForm />
               </div>
