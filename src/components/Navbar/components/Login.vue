9c9b958b047f17fcb1ccd32056e4bc8802ead457
420045389 <420045389@qq.com>
2024-05-08 17:10:03 +0800

    ✨ feat:消除i18n警告

--- src/components/Navbar/components/Login.vue
+++ src/components/Navbar/components/Login.vue
@@ -1,4 +1,30 @@
+<i18n lang="json5">
+  {
+    en: {
+      "switchRoles": "Switch Roles",
+      "userCenter": "User Center",
+      "userSettings": "User Settings",
+      "logout": "Logout",
+      "settings": "Settings",
+      "InfoNotification": "Info Notification",
+      "logOutContent": "Are you sure you want to log out?",
+
+    },
+    zh: {
+      "switchRoles": "切换角色",
+      "userCenter": "用户中心",
+      "userSettings": "用户设置",
+      "logout": "登出登录",
+      "settings": "设置",
+      "InfoNotification": "信息通知",
+      "logOutContent": "您确定要退出登录吗？",
+
+    }
+  }
+</i18n>
+
 <script lang='jsx'>
+import { Modal } from '@arco-design/web-vue'
 import { useRouter } from 'vue-router'
 import { clearToken } from '@/utils/auth'
 
@@ -55,8 +81,24 @@ export default defineComponent({
                   </a-doption>
                   <a-doption>
                     <a-space onClick={() => {
-                      clearToken()
-                      router.push('/login')
+                      Modal.warning({
+                        width: 350,
+                        title: t('InfoNotification'),
+                        hideCancel: false,
+                        content: () => {
+                          return (
+                            <>
+                              <div className="w-full flex items-center justify-center text-center">
+                                {t('logOutContent')}
+                              </div>
+                            </>
+                          )
+                        },
+                        onOk: () => {
+                          clearToken()
+                          router.push('/login')
+                        },
+                      })
                     }}
                     >
                       <icon-export />
