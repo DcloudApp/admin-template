be15aac6d59313ec84bd745f11e2054e64f549af
420045389 <420045389@qq.com>
2024-07-02 19:02:26 +0800

    ✨ feat:主题更改添加效果

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <script  lang='jsx'>
 import enUS from '@arco-design/web-vue/es/locale/lang/en-us'
 import zhCN from '@arco-design/web-vue/es/locale/lang/zh-cn'
-import { watch } from 'vue'
 
 export default defineComponent({
   setup() {
     const { locale } = useI18n()
-    const isDark = useDark()
-    watch(() => isDark.value, (newVal) => {
-      if (newVal)
-        document.body.setAttribute('arco-theme', 'dark')
-      else
-        document.body.removeAttribute('arco-theme')
-    }, { immediate: true })
 
     const locales = {
       zh: zhCN,
--- src/components/Navbar/components/Dark.vue
+++ src/components/Navbar/components/Dark.vue
@@ -3,8 +3,54 @@ export default defineComponent({
   props: {},
   emits: [],
   setup() {
-    const isDark = useDark()
+    const isDark = useDark({
+      selector: 'html',
+      attribute: 'theme',
+      valueDark: 'dark',
+      valueLight: 'light',
+    })
+
     const toggleDark = useToggle(isDark)
+
+    watch(() => isDark.value, (newVal) => {
+      if (newVal)
+        document.body.setAttribute('arco-theme', 'dark')
+      else
+        document.body.removeAttribute('arco-theme')
+    }, { immediate: true })
+
+    const toggleTheme = (event) => {
+      // 兼容性处理
+      if (!document.startViewTransition) {
+        toggleDark()
+        return
+      }
+      const x = event.clientX
+      const y = event.clientY
+      const endRadius = Math.hypot(
+        Math.max(x, innerWidth - x),
+        Math.max(y, innerHeight - y),
+      )
+      const transition = document.startViewTransition(async () => {
+        toggleDark()
+      })
+
+      transition.ready.then(() => {
+        const clipPath = [`circle(0px at ${x}px ${y}px)`, `circle(${endRadius}px at ${x}px ${y}px)`]
+        document.documentElement.animate(
+          {
+            clipPath: isDark.value ? [...clipPath].reverse() : clipPath,
+          },
+          {
+            duration: 500,
+            easing: 'ease-in',
+            pseudoElement: isDark.value
+              ? '::view-transition-old(root)'
+              : '::view-transition-new(root)',
+          },
+        )
+      })
+    }
 
     return () => (
       <>
@@ -12,8 +58,8 @@ export default defineComponent({
           size="medium"
           shape="circle"
           status="normal"
-          onClick={() => {
-            toggleDark()
+          onClick={(e) => {
+            toggleTheme(e)
           }}
         >
           {{
@@ -31,3 +77,27 @@ export default defineComponent({
   },
 })
 </script>
+
+<style  lang="scss">
+::view-transition-old(root),
+::view-transition-new(root) {
+  animation: none;
+  mix-blend-mode: normal;
+}
+
+::view-transition-old(root) {
+  z-index: 1;
+}
+
+::view-transition-new(root) {
+  z-index: 2147483646;
+}
+
+[theme='dark']::view-transition-old(root) {
+  z-index: 2147483646;
+}
+
+[theme='dark']::view-transition-new(root) {
+  z-index: 1;
+}
+</style>
